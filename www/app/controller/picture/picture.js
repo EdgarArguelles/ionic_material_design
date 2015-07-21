8c24bb2288a7b5f2329ed2729c115c32fcefbdbb
edgar <ing.edgar.arguelles@gmail.com>
2015-07-21 15:40:41 -0500

    complete example of camera

--- www/app/controller/picture/picture.js
+++ www/app/controller/picture/picture.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-    angular.module('app.controller.picture', [])
+    angular.module('app.controller.picture', ['app.service.refresh-svc'])
         .config(config)
         .controller('PictureCtrl', PictureCtrl);
 
@@ -18,16 +18,29 @@
     }
 
     // @ngInject
-    function PictureCtrl($mdDialog) {
+    function PictureCtrl($scope, $mdDialog, RefreshSvc) {
+        this._$scope = $scope;
         this._$mdDialog = $mdDialog;
-        this.pictures = [];
+        this._refreshSvc = RefreshSvc;
+        this.pictures = [
+            'http://www.independent.co.uk/incoming/article8465213.ece/alternates/w620/v2-cute-cat-picture.jpg',
+            'http://www.sharebin.net/gHc.jpg',
+            'http://hdwallnpics.com/wp-content/gallery/hd-tiger-images/tigers-wallpapers-hd.jpg',
+            'http://3.bp.blogspot.com/-rZmCIp0C-hQ/Tx6aCFeweoI/AAAAAAAAAnQ/WqIEVBTIzRk/s1600/Cool-Tiger-Wallpaper-1920x1080-HD.jpg',
+            'http://economictimes.indiatimes.com/thumb/msid-25252601,width-640,resizemode-4/stunning-images-of-the-space.jpg'
+        ];
     }
 
     PictureCtrl.prototype = {
+        doRefresh: function () {
+            this._$scope.$broadcast('scroll.refreshComplete');
+        },
         new: function () {
+            //only works on device
             var self = this;
             navigator.camera.getPicture(function (url) {
                 self.pictures.push(url);
+                self._refreshSvc.triggerRefresh('refresh-content');
             });
         },
         remove: function (index) {
